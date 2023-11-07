6e09d4d92c5b70e488460df84d18fda630160102
Brandon Jadotte <brandon@onefunction.tech>
2023-11-07 11:14:31 -0500

    Initial Commit

---
+++ Recipe Browser/Recipe_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BrowserApp.swift
+//  Recipe Browser
+//
+//  Created by Brandon Jadotte on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe BrowserUITests/Recipe_BrowserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Recipe_BrowserUITestsLaunchTests.swift
+//  Recipe BrowserUITests
+//
+//  Created by Brandon Jadotte on 11/7/23.
+//
+
+import XCTest
+
+final class Recipe_BrowserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
